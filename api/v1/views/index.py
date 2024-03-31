5cbcfd03511b8acb55ff5d715a391db731ede706
Omarkh2021 <90703502+Omarkh2021@users.noreply.github.com>
2024-03-31 18:06:20 +0200

    Update index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,37 +1,28 @@
 #!/usr/bin/python3
-"""
-Flask route that returns json status response
-"""
+""" This is the index file for  flask app"""
+
 from api.v1.views import app_views
-from flask import jsonify, request
+from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status', methods=['GET'])
-def status():
-    """
-    function for status route that returns the status
-    """
-    if request.method == 'GET':
-        resp = {"status": "OK"}
-        return jsonify(resp)
+@app_views.route('/status', strict_slashes=False)
+def api_status():
+    """a function to return api status"""
+
+    return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'])
-def stats():
-    """
-    function to return the count of all class objects
-    """
-    if request.method == 'GET':
-        response = {}
-        PLURALS = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-        for key, value in PLURALS.items():
-            response[value] = storage.count(key)
-        return jsonify(response)
+@app_views.route('/stats', strict_slashes=False)
+def obj_stats():
+    """returns the number of each object"""
+
+    my_dict = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(my_dict)
